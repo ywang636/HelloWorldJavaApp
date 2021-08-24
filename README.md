a8ad2358a835c621c76549a465d641930e99226a
Yiwen Wang <yiwenwa@amazon.com>
2021-08-24 16:52:43 -0700

    HelloWorld init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Build Command
+```aidl
+mvn clean package
+```
+
+Run Command
+```aidl
+java -jar target/HelloWorldJavaApp-1.0-SNAPSHOT.jar -Xms256m .
+```
+
+Port
+```aidl
+8080
+```
+Service URL
+```aidl
+http://localhost:8080/
+```
---
+++ src/main/java/com/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.HelloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index(){
+        String s = "Hello World";
+        return s;
+    }
+}
